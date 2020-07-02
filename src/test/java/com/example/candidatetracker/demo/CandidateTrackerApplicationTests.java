105aa2d058ec4f2938425732391d543a480cebb0
Harsh Patel <harsh.patel4@somaiya.edu>
2020-07-03 01:29:07 +0530

    Added Spring Boot starter files.

---
+++ src/test/java/com/example/candidatetracker/demo/CandidateTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candidatetracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
