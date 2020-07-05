2026dd7cbb46cc2fa379d910fa40c644ceecfd4d
Harsh Patel <harsh.patel4@somaiya.edu>
2020-07-05 13:56:07 +0530

    merged master with entities

---
+++ src/main/java/com/example/candidatetracker/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.candidatetracker.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String homePage(){
+        return "Home Page";
+    }
+}
