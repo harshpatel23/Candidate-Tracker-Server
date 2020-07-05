22eb7321fcca38130738e0f8f4ed75ee6cd2d61b
hitanshu310 <hitanshu98@gmail.com>
2020-07-05 10:58:54 +0530

    Added 2 entities yet to resolve inter entity issues

--- src/main/java/com/example/candidatetracker/demo/CandidateTrackerApplication.java
+++ src/main/java/com/example/candidatetracker/demo/CandidateTrackerApplication.java
@@ -1,13 +1,45 @@
 package com.example.candidatetracker.demo;
 
+import com.example.candidatetracker.demo.entity.Role;
+import com.example.candidatetracker.demo.entity.User;
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@RestController
 public class CandidateTrackerApplication {
+
+	private EntityManager entityManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CandidateTrackerApplication.class, args);
+	}
+
+	public CandidateTrackerApplication(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Transactional
+	@GetMapping("/hello")
+	public Role sayHello() {
+		Session session = entityManager.unwrap(Session.class);
+		User user = new User("hitanshu98@gmail.com", "asdfghj","Hitanshu", "Shah", "9869710860", true);
+		Role role = session.get(Role.class, "ADMIN");
+		//user.setRole(role);
+		//user.setManager(null);
+		//session.saveOrUpdate(user);
+		session.close();
+		//role.getUsers().add(user);
+		//Query<User> query = session.createQuery("from User", User.class);
+		//List<User> userList = query.getResultList();
+		//session.close();
+		return role;
 	}
 
 }
