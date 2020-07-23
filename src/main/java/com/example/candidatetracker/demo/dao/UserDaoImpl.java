c24b1b4a1e4d9880428704ff16eef695c28d2656
hitanshu <hitanshu98@gmail.com>
2020-07-23 12:10:20 +0530

    Added save and edit interviewer skills'

--- src/main/java/com/example/candidatetracker/demo/dao/UserDaoImpl.java
+++ src/main/java/com/example/candidatetracker/demo/dao/UserDaoImpl.java
@@ -1,17 +1,9 @@
 package com.example.candidatetracker.demo.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.transaction.Transactional;
-
 import com.example.candidatetracker.demo.entity.PasswordData;
+import com.example.candidatetracker.demo.entity.Skill;
 import com.example.candidatetracker.demo.entity.User;
 import com.example.candidatetracker.demo.service.EmailService;
-
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +12,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Repository
-public class UserDaoImpl implements UserDAO{
+public class UserDaoImpl implements UserDAO {
 
     private EntityManager entityManager;
+
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private EmailService emailService;
 
     @Autowired
-    public UserDaoImpl(EntityManager entityManager, BCryptPasswordEncoder bCryptPasswordEncoder, EmailService emailService){
+    public UserDaoImpl(EntityManager entityManager, BCryptPasswordEncoder bCryptPasswordEncoder, EmailService emailService) {
         this.entityManager = entityManager;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.emailService = emailService;
@@ -40,63 +41,63 @@ public class UserDaoImpl implements UserDAO{
 
         int userId = current_user.getId();
         Session session = entityManager.unwrap(Session.class);
-        
-        Query<User> query = session.createQuery("select u from User u where u.id = :userId", User.class).setParameter("userId",userId);
+
+        Query<User> query = session.createQuery("select u from User u where u.id = :userId", User.class).setParameter("userId", userId);
 
         User user = query.getSingleResult();
 
         List<User> successors = user.getSuccessors().stream().sorted((u1, u2) -> Integer.valueOf(u1.getId()).compareTo(Integer.valueOf(u2.getId()))).collect(Collectors.toList());
 
-        return new ResponseEntity<>(successors, HttpStatus.OK);    
+        return new ResponseEntity<>(successors, HttpStatus.OK);
     }
 
     @Override
-	public ResponseEntity<User> findById(int id) throws Exception {
+    public ResponseEntity<User> findById(int id) throws Exception {
 
         Session session = entityManager.unwrap(Session.class);
 
         User user = session.get(User.class, id);
 
         return user != null ? new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
-	}
+    }
 
     @Override
-	public ResponseEntity<User> findByEmail(String email) throws Exception {
-        
+    public ResponseEntity<User> findByEmail(String email) throws Exception {
+
         Session session = entityManager.unwrap(Session.class);
-        
+
         Query<User> query = session.createQuery("select u from User u where u.email = :email", User.class).setParameter("email", email);
 
-        User user; 
-        try{
+        User user;
+        try {
             user = query.getSingleResult();
-        }catch(NoResultException e){
+        } catch (NoResultException e) {
             user = null;
         }
 
         return user != null ? new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
 
-	}
+    }
 
     @Override
     public ResponseEntity<List<User>> findByRole(String role, User current_user) throws Exception {
 
         int userId = current_user.getId();
-            
+
         Session session = entityManager.unwrap(Session.class);
-        
-        Query<User> query = session.createQuery("select u from User u where u.id = :userId", User.class).setParameter("userId",userId);
+
+        Query<User> query = session.createQuery("select u from User u where u.id = :userId", User.class).setParameter("userId", userId);
 
         User user = query.getSingleResult();
-        
+
         List<User> userList = user.getSuccessors().stream().filter(u -> u.getRole().getRole().equals(role)).collect(Collectors.toList());
 
         return new ResponseEntity<>(userList, HttpStatus.OK);
 
     }
-    
+
     @Override
-	public ResponseEntity<User> save(User user) throws Exception {
+    public ResponseEntity<User> save(User user) throws Exception {
 
         System.out.println(user);
 
@@ -104,27 +105,34 @@ public class UserDaoImpl implements UserDAO{
 
         //check if user email already exists
         User userWithSameEmail = findByEmail(user.getEmail()).getBody();
-        if(userWithSameEmail != null){
+        if (userWithSameEmail != null) {
             return new ResponseEntity<>(null, HttpStatus.CONFLICT);
         }
 
         String password = User.generateRandomPassword();
-        
+
         //Send mail to user about this password
-        try{
+        try {
             emailService.sendEmail(user.getEmail(), password);
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Some error occured");
             return new ResponseEntity<>(null, HttpStatus.FAILED_DEPENDENCY);
         }
 
-        String encryptedPassword = bCryptPasswordEncoder.encode(password); 
+        String encryptedPassword = bCryptPasswordEncoder.encode(password);
         user.setPassword(encryptedPassword);
         user.setIsActive(1);
+        for (Skill s : user.getSkills()) {
+            Skill sessionSkill = session.get(Skill.class, s.getSkillId());
+            if (sessionSkill == null)
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            sessionSkill.getInterviewers().add(user);
+        }
+
         session.save(user);
-        
+
         return new ResponseEntity<>(user, HttpStatus.OK);
-	}
+    }
 
     @Override
     @Transactional
@@ -133,7 +141,7 @@ public class UserDaoImpl implements UserDAO{
 
         User existing_user = session.find(User.class, user.getId());
 
-        if(existing_user == null){
+        if (existing_user == null) {
             return new ResponseEntity<>(existing_user, HttpStatus.NOT_FOUND);
         }
 
@@ -146,10 +154,20 @@ public class UserDaoImpl implements UserDAO{
         existing_user.setLastName(user.getLastName());
         existing_user.setContact(user.getContact());
         existing_user.setIsActive(user.getIsActive());
-        
+        Query query = session.createQuery("from Skill", Skill.class);
+        List<Skill> skills = query.getResultList();
+        Set<Skill> skillSet = user.getSkills();
+        for (Skill s : skills) {
+            if (skillSet.contains(s))
+                s.getInterviewers().add(existing_user);
+            else if (!skillSet.contains(s))
+                s.getInterviewers().remove(existing_user);
+        }
+
+        existing_user.setSkills(skillSet);
         session.close();
 
-        return new ResponseEntity<>(existing_user,HttpStatus.OK);
+        return new ResponseEntity<>(existing_user, HttpStatus.OK);
     }
 
     @Override
@@ -162,7 +180,7 @@ public class UserDaoImpl implements UserDAO{
         String existingPassword = existingUser.getPassword();
 
 
-        if(bCryptPasswordEncoder.matches(passwordData.getOldPassword(), existingPassword)){
+        if (bCryptPasswordEncoder.matches(passwordData.getOldPassword(), existingPassword)) {
             existingUser.setPassword(bCryptPasswordEncoder.encode(passwordData.getNewPassword()));
             session.save(existingUser);
             return new ResponseEntity<>("Password Changed Successfully", HttpStatus.OK);
@@ -171,15 +189,15 @@ public class UserDaoImpl implements UserDAO{
     }
 
     @Override
-    public ResponseEntity<List<User>> getInterviewers(User user)  throws Exception{
+    public ResponseEntity<List<User>> getInterviewers(User user) throws Exception {
         Session session = entityManager.unwrap(Session.class);
         Query<User> query = session.createQuery("select u from User u where u.role = 'interviewer'");
 
-        if(user.getRole().getRole().equals("recruiter")){
+        if (user.getRole().getRole().equals("recruiter")) {
             return new ResponseEntity<>(query.list(), HttpStatus.OK);
         }
 
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
-    
+
 }
